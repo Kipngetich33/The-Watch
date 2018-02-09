03e77915b1c28074b62d06889b553d4450483adf
Kipngetich33 <khalifngeno@gmail.com>
2018-02-09 15:39:30 +0300

    create the neighborhood models and its tests

--- The_Watch/watch/models.py
+++ The_Watch/watch/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighborhood(models.Model):
+    '''
+    A class that defines the blueprint of a Neighborhood model
+    '''
+    neighborhood_name = models.CharField(max_length =30,null=True) 
+    neighborhood_location = models.CharField(max_length =30, null =True)
+    occupants_count = models.PositiveIntegerField(default=0)
+    admin_foreign_key = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
+
+    def __str__(self):
+        return self.neighborhood_name
+
+    def create_neighborhood(self):
+        '''
+        Methods that saves a new neighborhood
+        '''
+        self.save()
+
+    def delete_neighborhood(self):
+        '''
+        Methods that deletes an exiting neighborhood
+        '''
+        self.delete() 
+    
+    def update_neighborhood(self):
+        '''
+        Methods that updates an exiting neighborhood
+        '''
+        self.
+
+ 
+
+
+
--- The_Watch/watch/tests.py
+++ The_Watch/watch/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from . models import Neighborhood
 
-# Create your tests here.
+class NeighborTestClass(TestCase):
+    '''
+    A class that test the Image class model
+    '''
+
+    def setUp(self):
+        '''
+        method that runs at the begginning of each test
+        '''
+        self.neighborhood = Neighborhood(neighborhood_name = 'Lincon', neighborhood_location ='Nebraska', occupants_count = 1)
+
+    def test_isInstance(self):
+        '''
+        Checks is an object is an instance 
+        '''
+        self.assertTrue(isinstance(self.neighborhood,Neighborhood)) 
+
+    def test_create_neighborhood(self):
+        '''
+        Tests is the save_neighborhood function works
+        '''
+        self.neighborhood.save_neighborhood()
+        neighborhoods = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhoods)==1)
+
+    def test_delete_neighborhood(self):
+        '''
+        Test the delete_neighborhood methods
+        '''
+        self.neighborhood.save_neighborhood()
+        self.neighborhood2 = Neighborhood(neighborhood_name = 'Rochester', neighborhood_location ='Nebraska', occupants_count = 1)
+        self.neighborhood2.save()
+        self.neighborhood.delete_neighborhood()
+        neighborhoods = Neighborhood.objects.all()
+        self.assertTrue(len(neighborhoods)==1)
+
+
