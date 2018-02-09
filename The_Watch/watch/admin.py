e20f0d97893916a92e3a0c5258f9aa4e9d90314e
Kipngetich33 <khalifngeno@gmail.com>
2018-02-09 20:13:37 +0300

    add an env file

--- The_Watch/watch/admin.py
+++ The_Watch/watch/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import Business, User_Profile , Neighborhood
 
-# Register your models here.
+admin.site.register(Business)
+admin.site.register(User_Profile)
+admin.site.register(Neighborhood)
