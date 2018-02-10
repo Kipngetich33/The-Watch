736443f71bbcc973a5553dedc1f06be55ea7c4aa
Kipngetich33 <khalifngeno@gmail.com>
2018-02-10 16:39:55 +0300

    add registration form.html

--- The_Watch/The_Watch/urls.py
+++ The_Watch/The_Watch/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('watch.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
