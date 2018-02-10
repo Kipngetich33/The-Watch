f1addd3f71a15e2c0497550d4e76690b63835759
Kipngetich33 <khalifngeno@gmail.com>
2018-02-10 16:41:59 +0300

    add a login.html

--- The_Watch/watch/views.py
+++ The_Watch/watch/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required.
 
+@login_required(login_url='/accounts/login/')
 def landing(request):
     return render(request,'landing.html')
 
